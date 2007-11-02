4e5cdc80f6b63d267d907cc8d9c8fcdc31a83e4a
hakandilek <hakandilek@c84901c5-783a-0410-9d39-054f56b19ff1>
2007-11-02 18:16:38 +0000

    Fix test problem with r 118

--- mesken-weblib/src/test/java/org/xmdl/mesken/webapp/action/UserActionTest.java
+++ mesken-weblib/src/test/java/org/xmdl/mesken/webapp/action/UserActionTest.java
@@ -28,7 +28,7 @@ public class UserActionTest extends BaseActionTestCase {
         MockHttpServletRequest request = new MockHttpServletRequest("GET", "/editUser.html");
         ServletActionContext.setRequest(request);
         
-        action.setId("-1"); // regular user
+        action.setId("1"); // regular user
         assertNull(action.getUser());
         assertEquals("success", action.edit());
         assertNotNull(action.getUser());
@@ -85,7 +85,7 @@ public class UserActionTest extends BaseActionTestCase {
 
     public void testRemove() throws Exception {
         User user = new User("admin");
-        user.setId(-2L);
+        user.setId(2L);
         action.setUser(user);
         assertEquals("success", action.delete());
         assertFalse(action.hasActionErrors());
