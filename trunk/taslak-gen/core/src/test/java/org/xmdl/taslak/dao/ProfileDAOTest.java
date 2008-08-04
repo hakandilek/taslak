3f25cdff759e19192be6679f8d7c1e1c2a1e6917
hakandilek <hakandilek@c84901c5-783a-0410-9d39-054f56b19ff1>
2008-08-04 15:47:38 +0000

    Add new generated classes with support for new embedded and virtual classes

---
+++ trunk/taslak-gen/core/src/test/java/org/xmdl/taslak/dao/ProfileDAOTest.java
@@ -0,0 +1,94 @@
+
+package org.xmdl.taslak.dao;
+
+
+import java.util.*;
+
+import org.springframework.dao.DataAccessException;
+
+import org.xmdl.ida.lib.test.BaseDaoTestCase;
+
+import org.xmdl.taslak.model.*;
+
+import org.xmdl.taslak.model.search.*; 
+
+
+
+
+
+
+
+
+/**
+ *
+ * Profile DAO Test Case
+ *  
+ * $Id$
+ *
+ * @generated
+ */ 
+public class ProfileDAOTest extends BaseDaoTestCase {
+
+    /**
+     * @generated
+     */ 
+    private ProfileDAO profileDAO = null;
+
+    /**
+     * @generated
+     */ 
+    public void setProfileDAO(ProfileDAO profileDAO) {
+        this.profileDAO = profileDAO;
+    }
+
+    /**
+     * @generated
+     */ 
+    public void testAddAndRemoveProfile() throws Exception {
+        Profile profile = new Profile();
+
+        profile.setPrivatePhone("HMrrM6nArUp SJc");
+
+        profile = profileDAO.save(profile);
+        flush();
+
+        profile = profileDAO.get(profile.getId());
+
+        assertNotNull(profile.getId());
+        assertEquals("HMrrM6nArUp SJc", profile.getPrivatePhone());
+
+        log.debug("removing profile...");
+        profileDAO.remove(profile.getId());
+        flush();
+
+        try {
+            profileDAO.get(profile.getId());
+            fail("Profile found in database");
+        } catch (DataAccessException dae) {
+            log.debug("Expected exception: " + dae.getMessage());
+            assertNotNull(dae);
+        }
+    }
+
+    /**
+     * @generated
+     */ 
+    public void testSearch() throws Exception {
+        ProfileSearch search = new ProfileSearch();
+        search.setPrivatePhone("HMrrM6nArUp SJc");
+        Collection<Profile> profiles = profileDAO.search(search);
+        assertTrue(profiles.size() > 0);
+    }
+
+    /**
+     * @generated
+     */ 
+    protected void onSetUpBeforeTransaction() throws Exception {
+        super.onSetUpBeforeTransaction();
+
+        Profile profile = new Profile();
+        profile.setPrivatePhone("HMrrM6nArUp SJc");
+        profileDAO.save(profile);
+    }
+
+}
